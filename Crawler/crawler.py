7ada8362dc653f23eb10c525c4e5f944e6f471b7
Prarthana Patel <prpatel@prpatel012970.starbucks.net>
2018-10-22 19:22:53 -0700

    Changes1

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -1,19 +1,25 @@
 from elasticsearch import Elasticsearch
+from bs4 import BeautifulSoup
 import requests
+import pprint
+
+
 class Crawler:
-
-    def __init__(self):
-        self.globalname = "Prarthana"
-
-    def print_name(self, name: str) -> int or None:
-        print("Name is ", name)
-        print("Global name ", self.globalname)
-        return
+    def get_data(self,url):
+        r = requests.get("http://" + url)
+        count = 0
+        data = r.text
+        soup = BeautifulSoup(data, "html.parser")
+        for link in soup.find_all('a'):
+            if "Java_Programming" in str(link.get('href')):
+                print(link.get('href'))
+                count+=1
+        print("No of links{0}sdv jsh{1}".format(count, "radsfs"))
 
 
 if __name__ == "__main__":
     es = Elasticsearch(['https://search-esaw2-s5pd4hrrxvewzfk2f6kezyg5mi.us-east-2.es.amazonaws.com/'])
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(es.info())
     craw = Crawler()
-    name = input("Enter name")
-    value = craw.print_name(name)
-    print(value)
+    craw.get_data("en.wikibooks.org/wiki/Java_Programming")
